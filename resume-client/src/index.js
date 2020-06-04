9c92851813e7b86f471eb5dff5d2b78958543bb7
Craig bauer <craigbauer23@gmail.com>
2020-06-04 15:38:23 -0400

    heroku config

--- resume-client/src/index.js
+++ resume-client/src/index.js
@@ -4,3 +4,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('dashboard'));
+
+//npm start
+//node app.js || npm start || npm run start
+//npm run sass
+//git subtree push --prefix path/to/subdirectory heroku master
