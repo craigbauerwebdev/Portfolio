00f38458f9263c8fe809d4c634790f3a3b3a2463
Craig Bauer <craigbauer23@gmail.com>
2022-03-08 15:00:34 -0500

    linter

--- resume-client/src/Header.js
+++ resume-client/src/Header.js
@@ -1,33 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 //import Button from './ui-components/Button';
-import Icons from './ui-components/Icons';
+import Icons from "./ui-components/Icons";
 import { Link } from "react-router-dom";
 import { AuthContext } from "./Auth/Auth";
 //import Logo from 'process.env.PUBLIC_URL + 'assets/CB-logo.png';
 
 const Header = (props) => {
   //render() {
-    const { currentUser } = useContext(AuthContext);
-    //console.log(currentUser);
-    return (
-      <div className="header">
-        <header className="header">
-          <Link to={`${process.env.PUBLIC_URL}/`}>
-            <img alt="CBD Logo" height="100" src={`${process.env.PUBLIC_URL}/assets/CB-logo.png`} />
-          </Link>
-          <Link to="/login">
-            <div className="settings-icon">
-              <i className="large material-icons">settings</i>
-              {currentUser &&
-                <span className="welcome">Signed In</span>
-              }
-            </div>
-          </Link>
-          <Icons showButtons={true} />
-          
-        </header>
-      </div>
-    );
+  const { currentUser } = useContext(AuthContext);
+  //console.log(currentUser);
+  return (
+    <div className="header">
+      <header className="header">
+        <Link to={`${process.env.PUBLIC_URL}/`}>
+          <img
+            alt="CB Logo"
+            height="100"
+            src={`${process.env.PUBLIC_URL}/assets/CB-logo.png`}
+          />
+        </Link>
+        <Link to="/login">
+          <div className="settings-icon">
+            <i className="large material-icons">settings</i>
+            {currentUser && <span className="welcome">Signed In</span>}
+          </div>
+        </Link>
+        <Icons showButtons={true} />
+      </header>
+    </div>
+  );
   //}
-}
+};
 export default Header;
